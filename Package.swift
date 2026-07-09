ca0781ab201e6b87f511d4eaf22ec9d603d5ba9e
m2构造机 <zhanggaoqi@aobi.com>
2026-07-09 19:11:13 +0800

    [Branch Release] v2.0.3-dev-1482626 - develop
    - 更新所有 XCFrameworks 到 2.0.3-dev-1482626
    - 更新资源文件
    - 更新 checksums
    - 分支测试版本（无 tag）

--- Package.swift
+++ Package.swift
@@ -36,18 +36,18 @@ let package = Package(
         // ========== Binary Frameworks ==========
         .binaryTarget(
             name: "ImageUploadUnityWrap",
-            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/ImageUploadUnityWrap/2.0.3-dev-1481866/ImageUploadUnityWrap.xcframework.zip",
-            checksum: "a4661a291991d2577b9f339b7cacf0f8cfe52f04a5047d51572d7f755fc571f9"
+            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/ImageUploadUnityWrap/2.0.3-dev-1482626/ImageUploadUnityWrap.xcframework.zip",
+            checksum: "bec4556331480d7c96a46a974681814042395f2c86663339d17d4d198c0e5f67"
         ),
         .binaryTarget(
             name: "QCloudCore",
-            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/ImageUploadUnityWrap/2.0.3-dev-1481866/QCloudCore.xcframework.zip",
-            checksum: "8a3b0a222ce0f0fece8a450895752d204fa68e569ce72c5ac89896d95a529bc7"
+            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/ImageUploadUnityWrap/2.0.3-dev-1482626/QCloudCore.xcframework.zip",
+            checksum: "72fe82fd6da546298dcc4822b86dcf541dbb88183f478b04eac6da8e095333ff"
         ),
         .binaryTarget(
             name: "QCloudCOSXML",
-            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/ImageUploadUnityWrap/2.0.3-dev-1481866/QCloudCOSXML.xcframework.zip",
-            checksum: "50804209d5af7122a373f046d54f4df6225a5f8fad40eb34eae4dd4f171d293c"
+            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/ImageUploadUnityWrap/2.0.3-dev-1482626/QCloudCOSXML.xcframework.zip",
+            checksum: "36e424eed82a8bd36f08fe83a11ccbb322e45a0721e65c9bf0ee5db4adc5fc79"
         )
     ]
 )
